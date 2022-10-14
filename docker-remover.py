4f98bd455674903f9bb68c1a09b37a56ddb1401c
darius <darius@bit-sentinel>
2022-10-14 16:55:04 +0300

    add script

---
+++ docker-remover.py
@@ -0,0 +1,38 @@
+import docker
+
+docker = docker.from_env()
+
+def dispay_image():
+    list = docker.images.list(all=True)
+    return list
+
+def remove_image():
+    img = docker.images.prune(filters={'dangling': False})
+    return img
+
+def remove_containers_inactive():
+    cont = docker.containers.prune()
+    return cont
+
+def remove_containers_inactive():
+    net = docker.networks.prune()
+    return net
+
+def remove_volumes():
+    vol = docker.volumes.prune()
+    return vol 
+
+def remove_containers_active():
+    containers = docker.containers.list(all=True)
+
+    for cont in containers:
+        print(containers)
+        cont.remove(force=True)
+
+
+if __name__ == '__main__':
+    print(dispay_image())
+    print(remove_containers_active())
+    print(remove_volumes())
+    print(remove_containers_inactive())
+    print(remove_image())
