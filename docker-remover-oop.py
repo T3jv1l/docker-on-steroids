28d7b262a4d2bc8c39cee755ed8888111d21ccbe
MrLeritaite <Mr.Leritaite@protonmail.com>
2022-10-20 10:48:06 +0300

    Purging all the dockers and rewrite the arguments

--- docker-remover-oop.py
+++ docker-remover-oop.py
@@ -9,13 +9,17 @@ from dotenv import load_dotenv
 
 class DockeronSteroids():
     def __init__(self) -> None:
+        """Construct all the necessary attributes"""
         load_dotenv()
+        self.docker_env = docker.from_env()
         self.parser = argparse.ArgumentParser()
 
     def __setArgs(self):
-
-        self.parser.add_argument('-all',type=str, required=False, help="purge all docker containers", metavar='purge')
-        self.parser.add_argument('-search',type=str, required=False, help='search all docker-compose.yml file', metavar='docker-compose.yml')
+        """Setting the arguments"""
+        self.parser.add_argument('-s',type=str, required=False,
+                     help='search all docker-compose.yml file', metavar=' --search')
+        self.parser.add_argument("-a", "--all" ,help="purge all docker containers",
+                    action="store_true")
 
         subparser = self.parser.add_subparsers(dest='remote')
         
@@ -25,16 +29,72 @@ class DockeronSteroids():
         ssh.add_argument('--port', type=str ,required=True, help="port require", metavar='22')
 
     def __check(self):
+        """Checking the number of arguments"""
         if len(sys.argv) <= 1:
             sys.argv.append('--help')
+
+    def __display_image(self):
+        """Displaying all the docker images"""
+        list = self.docker_env.images.list(all=True)
+
+        if list == []:
+            return "{'Image list is empty': None}"
+        else:
+            return list 
+
+    def __remove_image(self):
+        """Removing the docker image"""
+        img = self.docker_env.images.prune(filters={'dangling': False})
+        return img
+
+    def __remove_containers_inactive(self):
+        """Removing the inactive docker container"""
+        cont = self.docker_env.containers.prune()
+        return cont 
+
+    def __remove_networks(self):
+        """Removing the docker network"""
+        net = self.docker_env.networks.prune()
+        return net
+
+    def __remove_volumes(self):
+        """Removing the docker volumes"""
+        time.sleep(0.5)
+        vol = self.docker_env.volumes.prune()
+        return vol 
+
+    def __remove_containers_active(self):
+        """Removing the active docker container"""
+        containers = self.docker_env.containers.list(all=True)
+        
+        if containers == []:
+            return "{'Active Container list is empty': None}"
+        else:
+            for cont in containers:
+                cont.remove(force=True)
+            return containers
+    
+    def __remove(self):
+        """Calling all the functions responsable for purging the docker"""
+        print(self.__display_image())
+        print(self.__remove_networks())
+        print(self.__remove_containers_inactive())
+        print(self.__remove_volumes())
+        print(self.__remove_containers_active())
+        print(self.__remove_image())
+
+    def __arguments(self, options):
+        print(options)
+        if options.all:
+            self.__remove()
+            exit()
 
     def run(self):
         self.__setArgs()
         self.__check()
         self.options = self.parser.parse_args()
+        self.__arguments(self.options)
 
 if __name__ == '__main__':
-    load_dotenv()
-
     test = DockeronSteroids()
     test.run()
